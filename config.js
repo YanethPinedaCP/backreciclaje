7839d772485130c77701d06450a9004a66b42ecb
Yaneth Pineda <cleimisyanethpineda@gmail.com>
2025-11-19 02:55:44 -0600

    Actualización del backend

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'db_separapp',
+    database: process.env.DB_DATABASE || 'railway',
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
